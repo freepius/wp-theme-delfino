7aa50f855db8b3848b0f89a9fb5501318962f7cc
freepius <contact@freepius.net>
2021-01-03 18:00:40 +0100

    Add the "collapser" capability: <h2>'s child-elements are collapsed

---
+++ main.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  makeOnlyOneChildPerH2()
+
+  const allH2 = document.querySelectorAll('main.collapser > h2')
+
+  // collapse on <h2> click
+  allH2.forEach(h2 => h2.addEventListener('click', e =>
+    allH2.forEach(h2 => e.target !== h2
+      ? h2.classList.remove('active')
+      : document.documentElement.clientWidth > 850 ? h2.classList.add('active') : h2.classList.toggle('active')
+    )
+  ))
+})
+
+/**
+ * If a <h2> element has several children, group them in a <section> element.
+ */
+function makeOnlyOneChildPerH2 () {
+  let h2 = document.querySelector('main.collapser > h2:first-of-type')
+  let e = h2.nextElementSibling
+  let section = document.createElement('section')
+
+  const flushSection = function (h2, section) {
+    if (section.children.length > 0) {
+      h2.insertAdjacentElement('afterend', section.children.length > 1 ? section : section.children[0])
+    }
+  }
+
+  // make the first <h2> & child-element active
+  h2.classList.add('active')
+
+  while (e) {
+    if (e.nodeName === 'H2') {
+      // new <h2> found -> flush the current <section>
+      flushSection(h2, section)
+
+      h2 = e
+      e = e.nextElementSibling
+      section = document.createElement('section')
+    } else {
+      const next = e.nextElementSibling
+      section.appendChild(e)
+      e = next
+    }
+  }
+
+  // flush the last <section>
+  flushSection(h2, section)
+}
