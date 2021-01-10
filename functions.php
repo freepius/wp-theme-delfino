4f57941c800f73f244f7b62a14d9e547a6e0ad73
freepius <contact@freepius.net>
2021-01-10 23:22:38 +0100

    Small scss improvements (for header and collapser)

--- functions.php
+++ functions.php
@@ -61,10 +61,8 @@ function delfino_widgets_init() {
 		'name'          => esc_html__( 'Header content', 'delfino' ),
 		'id'            => 'header-content',
 		'description'   => esc_html__( 'Add your header content here.', 'delfino' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_sidebar' => '<section id="%1$s">',
+		'after_sidebar' => '</section>',
 	]);
 }
 add_action( 'widgets_init', 'delfino_widgets_init' );
